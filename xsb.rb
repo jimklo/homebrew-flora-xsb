a624136651576d28990ac70a28f4e7643cbf36e4
Jim Klo <jim.klo@sri.com>
2014-11-17 23:18:20 -0800

    install patched XSB and build dymodule
    Signed-off-by: Jim Klo <jim.klo@sri.com>

--- xsb.rb
+++ xsb.rb
@@ -1,23 +1,37 @@
 require "formula"
 
 class Xsb < Formula
-  homepage "http://www.xsb.com"
-  head "http://svn.code.sf.net/p/xsb/src/trunk"
+  homepage "http .xsb.com"
+  head "http://svn.code.sf.net/p/xsb/src/trunk/XSB"
 
-  depends_on "makedepend" => :optional
+  depends_on "makedepend"
   depends_on "pcre"
   depends_on "curl"
   depends_on "libxml2"
+  depends_on "autoconf"
+  depends_on "automake"
 
   keg_only "XSB is Keg Only"
-
+  
+  # Need to patch configure.in because the use of .jnilib for modules on OS is deprecated.
+  patch :p0 do
+    url "https://raw.githubusercontent.com/jimklo/homebrew-flora-xsb/master/xsb-configure.patch"
+    sha1 "5f878ed7d07d9ff10390b9747bfa1fdc6c932795"
+  end
+  
   def install
     ENV.deparallelize  # if your formula fails when building in parallel
-    Dir.chdir("XSB/build")
+    Dir.chdir("build")
+
+    system "autoconf"
+
     # Remove unrecognized options if warned by configure
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--with-gcc"
     system "./makexsb", "clean"
     system "./makexsb" 
+
+    system "bash", "./makexsb", "dynmodule"
+
     system "./makexsb", "install" 
   end
 
